9462facde648bc88b07e55ec368f61ba21783e3b
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2023-03-14 15:44:32 +0100

    Added searching by days and multiple categories. Added sorting by date or category__name. Added total amount spent

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from .models import Expense
+from .models import Expense, Category
 
 
 class ExpenseSearchForm(forms.ModelForm):
+    from_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
+    to_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
+    categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
     class Meta:
         model = Expense
         fields = ('name',)
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.db.models import Sum
 from django.views.generic.list import ListView
 
 from .forms import ExpenseSearchForm
@@ -9,23 +12,56 @@ class ExpenseListView(ListView):
     model = Expense
     paginate_by = 5
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        sort = self.request.GET.get('sort')
+
+        if sort == 'category':
+            queryset = queryset.order_by('category__name')
+        elif sort == 'category_desc':
+            queryset = queryset.order_by('-category__name')
+        elif sort == 'date':
+            queryset = queryset.order_by('date')
+        elif sort == 'date_desc':
+            queryset = queryset.order_by('-date')
+
+        return queryset
+
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
 
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
+            start_date_str = form.cleaned_data.get('start_date', '').strip()
+            end_date_str = form.cleaned_data.get('end_date', '').strip()
+            categories = form.cleaned_data.get('categories')
+
             if name:
                 queryset = queryset.filter(name__icontains=name)
+            if start_date_str and end_date_str:
+                start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+                end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+                queryset = queryset.filter(date__range=(start_date, end_date))
+            elif start_date_str:
+                start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+                queryset = queryset.filter(date__gte=start_date)
+            elif end_date_str:
+                end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+                queryset = queryset.filter(date__lte=end_date)
+            elif categories:
+                queryset = queryset.filter(category__in=categories)
+
+        total_amount = queryset.aggregate(Sum('amount'))['amount__sum'] or 0
 
         return super().get_context_data(
             form=form,
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
+            total_amount=total_amount,
             **kwargs)
 
 
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
-
